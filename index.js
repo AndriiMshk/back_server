aeea8b9563e2efea7e611b99accb95bb0500f8bb
AndrMshk <andrmshk1@gmail.com>
2023-03-29 09:53:54 +0300

    add router

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { userRouter } from './users/users.js'
+
+
+const port = 8000
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.status(404).end()
+})
+
+app.use('/users', userRouter)
+
+app.listen(port, () => {
+    console.log('listen');
+})
