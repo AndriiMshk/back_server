982b50ed2de45123a1ad1dbda6774fadfded0222
AndrMshk <andrmshk1@gmail.com>
2023-04-08 11:10:00 +0300

    add test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
 import { inject, injectable } from 'inversify';
-import { ExeptionFilter } from './errors/exeption.filter';
 import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 import { json } from 'body-parser';
@@ -50,5 +49,9 @@ export class App {
 		await this.prismaService.connect();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
+
+	public close(): void {
+		this.server.close();
 	}
 }
